cfd7fba6047f7a8eb7ed339d445f247df3a51ad2
Jussi Tuomi <jussi.tuomi@protonmail.com>
2020-02-01 20:57:04 +0700

    Animator stuff, breakable stuff

--- Assets/Scripts/Breakable.cs
+++ Assets/Scripts/Breakable.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Breakable : MonoBehaviour
 {
+    [SerializeField] private BreakableConditionUI uiConditionPrefab;
     public int2 GridPosition;
     private float _health;
-
+    private BreakableConditionUI _conditionUi;
+    
     public float Health => _health;
     
     private void Start()
@@ -14,11 +16,19 @@ public class Breakable : MonoBehaviour
         GridPosition = transform.position.GetGridPosition();
         GameManager.Instance.level.MakeUnwalkable(GridPosition);
         transform.position = GridPosition.GetWorldPosition();
+        
+        GameObject ui = GameObject.Find("BreakableUI");
+        _conditionUi = Instantiate(uiConditionPrefab, ui.transform);
+        
         SetHealth(1.0f);
+        
+        _conditionUi.transform.localScale = Vector3.one;
+        _conditionUi.Initialize(this);
     }
 
     public void SetHealth(float hp)
     {
-        _health = hp;
+        _health = Mathf.Clamp(hp, 0, 1.0f);
+        _conditionUi.SetCondition(_health);
     }
 }
