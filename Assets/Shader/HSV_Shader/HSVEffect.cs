cee7a3b89be3df4d6afc272020c4ab85f6d06318
NattyboyZ <iamnattyboyz@hotmail.com>
2020-02-02 13:24:09 +0700

    Add HSV shader and change player2 color and sound

---
+++ Assets/Shader/HSV_Shader/HSVEffect.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace HSV
+{
+    [AddComponentMenu("Effects/HSV Adjust")]
+    [RequireComponent(typeof(SpriteRenderer)), DisallowMultipleComponent, ExecuteInEditMode]
+    public class HSVEffect : MonoBehaviour
+    {
+        public SpriteRenderer Renderer { get; private set; }
+        //public Color GlowColor
+        //{
+        //    get { return glowColor; }
+        //    set { if (glowColor != value) { glowColor = value; SetMaterialProperties(); } }
+        //}
+        //public float GlowBrightness
+        //{
+        //    get { return glowBrightness; }
+        //    set { if (glowBrightness != value) { glowBrightness = value; SetMaterialProperties(); } }
+        //}
+        //public int OutlineWidth
+        //{
+        //    get { return outlineWidth; }
+        //    set { if (outlineWidth != value) { outlineWidth = value; SetMaterialProperties(); } }
+        //}
+        //public float AlphaThreshold
+        //{
+        //    get { return alphaThreshold; }
+        //    set { if (alphaThreshold != value) { alphaThreshold = value; SetMaterialProperties(); } }
+        //}
+        //public bool DrawOutside
+        //{
+        //    get { return drawOutside; }
+        //    set { if (drawOutside != value) { drawOutside = value; SetMaterialProperties(); } }
+        //}
+        //public bool EnableInstancing
+        //{
+        //    get { return enableInstancing; }
+        //    set { if (enableInstancing != value) { enableInstancing = value; SetMaterialProperties(); } }
+        //}
+
+        public float Hue
+        {
+            get { return hue; }
+            set { if (hue != value) { hue = value; SetMaterialProperties(); } }
+        }
+        public float Sat
+        {
+            get { return sat; }
+            set { if (sat != value) { sat = value; SetMaterialProperties(); } }
+        }
+        public float Val
+        {
+            get { return val; }
+            set { if (val != value) { val = value; SetMaterialProperties(); } }
+        }
+        public bool EnableInstancing
+        {
+            get { return enableInstancing; }
+            set { if (enableInstancing != value) { enableInstancing = value; SetMaterialProperties(); } }
+        }
+
+        [Range(-360, 360)]
+        [SerializeField] private float hue = 0;
+        [Range(0, 10)]
+        [SerializeField] private float sat = 1;
+        [Range(0, 10)]
+        [SerializeField] private float val = 1;
+        [SerializeField] private bool enableInstancing = true;
+
+        //[Tooltip("Base color of the glow.")]
+        //[SerializeField] private Color glowColor = Color.white;
+        //[Tooltip("The brightness (power) of the glow."), Range(1, 10)]
+        //[SerializeField] private float glowBrightness = 2f;
+        //[Tooltip("Width of the outline, in texels."), Range(0, 10)]
+        //[SerializeField] private int outlineWidth = 1;
+        //[Tooltip("Threshold to determine sprite borders."), Range(0f, 1f)]
+        //[SerializeField] private float alphaThreshold = .01f;
+        //[Tooltip("Whether the outline should only be drawn outside of the sprite borders. Make sure sprite texture has sufficient transparent space for the required outline width.")]
+        //[SerializeField] private bool drawOutside = false;
+        //[Tooltip("Whether to enable GPU instancing.")]
+
+
+        private static readonly int hueId = Shader.PropertyToID("_HueShift");
+        private static readonly int saturationId = Shader.PropertyToID("_Sat");
+        private static readonly int valId = Shader.PropertyToID("_Val");
+        //private static readonly int alphaThresholdId = Shader.PropertyToID("_AlphaThreshold");
+
+        private MaterialPropertyBlock materialProperties;
+
+        private void Awake()
+        {
+            Renderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void OnEnable()
+        {
+            SetMaterialProperties();
+        }
+
+        private void OnDisable()
+        {
+            SetMaterialProperties();
+        }
+
+        private void OnValidate()
+        {
+            if (!isActiveAndEnabled) return;
+
+            // Update material properties when changing serialized fields with editor GUI.
+            SetMaterialProperties();
+        }
+
+        private void OnDidApplyAnimationProperties()
+        {
+            // Update material properties when changing serialized fields with Unity animation.
+            SetMaterialProperties();
+        }
+
+        private void SetMaterialProperties()
+        {
+            if (!Renderer) return;
+
+            Renderer.sharedMaterial = HSVMaterial.GetSharedFor(this);
+
+            if (materialProperties == null) // Initializing it at `Awake` or `OnEnable` causes nullref in editor in some cases.
+                materialProperties = new MaterialPropertyBlock();
+
+            //materialProperties.SetFloat(isOutlineEnabledId, isActiveAndEnabled ? 1 : 0);
+            materialProperties.SetFloat(hueId, hue);
+            materialProperties.SetFloat(saturationId, sat);
+            materialProperties.SetFloat(valId, val);
+
+            //materialProperties.SetColor(outlineColorId, GlowColor * GlowBrightness);
+            //materialProperties.SetFloat(outlineSizeId, OutlineWidth);
+            //materialProperties.SetFloat(alphaThresholdId, AlphaThreshold);
+
+            Renderer.SetPropertyBlock(materialProperties);
+        }
+
+
+        private void Reset()
+        {
+            SetMaterialProperties();
+        }
+    }
+}
---
+++ Assets/Shader/HSV_Shader/HSVMaterial.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HSV
+{
+    public class HSVMaterial : Material
+    {
+        public Texture SpriteTexture { get { return mainTexture; } }
+        //public bool DrawOutside { get { return IsKeywordEnabled(outsideMaterialKeyword); } }
+        public bool InstancingEnabled { get { return enableInstancing; } }
+
+        private const string hsvShaderName = "Custom/HSVShader";
+        //private const string outsideMaterialKeyword = "SPRITE_OUTLINE_OUTSIDE";
+        private static readonly Shader outlineShader = Shader.Find(hsvShaderName);
+
+        private static List<HSVMaterial> sharedMaterials = new List<HSVMaterial>();
+
+        public HSVMaterial(Texture spriteTexture, bool instancingEnabled = false)
+            : base(outlineShader)
+        {
+            if (!outlineShader) Debug.LogError(string.Format("{0} shader not found. Make sure the shader is included to the build.", hsvShaderName));
+
+            mainTexture = spriteTexture;
+            //if (drawOutside) EnableKeyword(outsideMaterialKeyword);
+            if (instancingEnabled) enableInstancing = true;
+        }
+
+        public static Material GetSharedFor(HSVEffect hsvEffect)
+        {
+            for (int i = 0; i < sharedMaterials.Count; i++)
+            {
+                if (sharedMaterials[i].SpriteTexture == hsvEffect.Renderer.sprite.texture &&
+                    //sharedMaterials[i].DrawOutside == spriteGlow.DrawOutside &&
+                    sharedMaterials[i].InstancingEnabled == hsvEffect.EnableInstancing)
+                    return sharedMaterials[i];
+            }
+
+            var material = new HSVMaterial(hsvEffect.Renderer.sprite.texture, hsvEffect.EnableInstancing);
+            material.hideFlags = HideFlags.DontSaveInBuild | HideFlags.DontSaveInEditor | HideFlags.NotEditable;
+            sharedMaterials.Add(material);
+
+            return material;
+        }
+    }
+}
